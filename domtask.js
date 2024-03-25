47905ac46594da366e948df5a77b4d8456e99925
Umavathi V <113404271+VUmavathi2002@users.noreply.github.com>
2024-03-25 10:33:38 +0530

    Add files via upload

---
+++ domtask.js
@@ -0,0 +1,21 @@
+
+document.getElementById("btnid1").addEventListener("click", demo=() => {
+    let button1 = document.getElementById("colid");
+    button1.style.backgroundColor = "#e0e0e0";
+   
+});
+document.getElementById("btnid2").addEventListener("click", demo=() => {
+    let button1 = document.getElementById("colid");
+    button1.style.backgroundColor = "#6fcf97";
+   
+});
+document.getElementById("btnid3").addEventListener("click", demo=() => {
+    let button1 = document.getElementById("colid");
+    button1.style.backgroundColor = "#56ccf2";
+   
+});
+document.getElementById("btnid4").addEventListener("click", demo=() => {
+    let button1 = document.getElementById("colid");
+    button1.style.backgroundColor = "#bb6bd9";
+   
+});
